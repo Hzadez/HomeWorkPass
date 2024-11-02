3a6a852976d96d77895ee4b4a4db1ba643fca391
Hzadez <hzadez63@gmail.com>
2024-11-02 14:29:48 +0400

    2ci Tapshirig

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string deyisen = Console.ReadLine(); 
+            string trimeden = Trimmetodu(deyisen);
+            Console.WriteLine(trimeden);
+
+            
+
+        }
+
+        static string Trimmetodu(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+
+            int start = 0;
+            int end = name.Length - 1;
+
+            while (start <= end && char.IsWhiteSpace(name[start]))
+            {
+                start++;
+            }
+
+            while (end >= start && char.IsWhiteSpace(name[end]))
+            {
+                end--;
+            }
+
+            return name.Substring(start, end - start + 1);
+
+        }
+    }
+}
